9f84363049464e4ddfb5650af89c93870830755c
Carl Glaysher <carl@coxeh.com>
2022-03-03 22:42:12 +0000

    add changes from detect

--- advantage/handlers.py
+++ advantage/handlers.py
@@ -5,6 +5,8 @@ from vantage_api.geometry import VantageGeometry
 import torch
 from yolov5.models.common import DetectMultiBackend
 from yolov5.utils.torch_utils import select_device, time_sync
+from yolov5.utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
+                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 
 class Verbose(PipelineHandler):
     def handle(self, task: VideoProcessingFrame, next):
@@ -43,14 +45,31 @@ class VideoWriter(PipelineHandler):
 class YoloProcessor(PipelineHandler):
     model = None
     device = None
-    def __init__(self, weights, device='') -> None:
+    half = None
+    def __init__(self, weights, device='', imgsz=(3072, 3072)) -> None:
         super().__init__()
-        self.device = select_device(device)
-        self.model = DetectMultiBackend(weights, device=self.device, dnn=False)
+        self.device = device = select_device(device)
+        self.model = model = DetectMultiBackend(weights, device=self.device, dnn=False)
+        stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
 
+        imgsz = check_img_size(imgsz, s=stride)  # check image size
+        # Half
+        self.half = half = (pt or jit or onnx or engine) and device.type != 'cpu'  # FP16 supported on limited backends with CUDA
+        if pt or jit:
+            model.model.half() if half else model.model.float()
+
+        bs=1
+        model.warmup(imgsz=(1 if pt else bs, 3, *imgsz), half=half)  # warmup
+            
     def handle(self, task: VideoProcessingFrame, next):
-        results = self.model(task.frame)
-        print(results)
+        im = torch.from_numpy(task.frame).to(self.device)
+        im = im.half() if self.half else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+    
+        pred = self.model(im)
+        print(pred)
         #task.put('yolo', 'test') 
         #print(task.get('yolo').results())
         return next(task)  
