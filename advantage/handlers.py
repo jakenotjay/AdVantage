2d779232b98d4f431dc5e0e7daf4513f769764fa
Carl Glaysher <carl@coxeh.com>
2022-03-06 10:19:15 +0000

    tweaks to runway detection

--- advantage/handlers.py
+++ advantage/handlers.py
@@ -191,8 +191,9 @@ class RunwayDetector(PipelineHandler):
         img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
         
         img_copy = img.copy()
+        offset_x, offset_y = self.findStablisedOffset(img)
         bin_img = self.convertImageToStandardisedBinary(img)
-        lines = self.findLinesInBinaryImage(bin_img, width)
+        lines = self.findLinesInBinaryImage(bin_img, width, offset_x, offset_y)
         img_copy = self.saveLinesToTask(task,img_copy, lines, width, height)
         img_copy, bin_img = self.saveTestImages(task, img_copy, bin_img)
 
@@ -209,7 +210,7 @@ class RunwayDetector(PipelineHandler):
             outputLines = []
             lines = sorted(lines, key=self.getLineLength)
             for i in range(0, len(lines)):
-                l = lines[i][0]
+                l = lines[i]
                 x1p = l[0] / width 
                 x2p = l[2] / width
                 y1p = l[1] / height
@@ -226,32 +227,110 @@ class RunwayDetector(PipelineHandler):
             task.put('runways', outputLines)
         return img    
 
-    def findLinesInBinaryImage(self, img, width):
+    def findLinesInBinaryImage(self, img, width, offset_x, offset_y, edge_offset=10):
         runway_length_drop_percentage = 10
         currentRunwayDropPercentage = runway_length_drop_percentage
         currentRunwayMin = width - (width * (currentRunwayDropPercentage / 100))
         lines = None
 
+        new_x = offset_y[0] + edge_offset
+        new_y = offset_x[0] + edge_offset
+
+        roi = img[new_x:img.shape[1] - offset_y[2] - edge_offset, new_y:img.shape[0] - offset_x[2] - edge_offset]
+
         while currentRunwayDropPercentage < 100:
-            lines = cv2.HoughLinesP(img, 1, np.pi / 180, 150, None, currentRunwayMin, 5)
+            lines = cv2.HoughLinesP(roi, 1, np.pi / 180, 20, None, currentRunwayMin, 10)
             if lines is not None:
                 break  
             currentRunwayDropPercentage += runway_length_drop_percentage
             currentRunwayMin = width - (width * (currentRunwayDropPercentage / 100))
+        new_lines = []
+        for i in range(0, len(lines)):
+            l = lines[i][0]
+            new_lines.append([
+                l[0] + new_y,
+                l[1] + new_x,
+                l[2] + new_y,
+                l[3] + new_x
+            ])
 
-        return lines    
+        return new_lines  
+
+    def findStablisedOffset(self, img):
+        check_points = 2
+        check_x = int(img.shape[1] / (check_points + 1))
+        check_y = int(img.shape[0] / (check_points + 1))
+        offset_y = [0,0,0,0]
+        offset_x = [0,0,0,0]
+        
+        broken = False
+        for y in range(img.shape[0]):
+            for p in range(check_points):
+                x = (p+1) * check_x
+                if img[x,y][0] == 0 and img[x,y][1] == 0:
+                    offset_x[p] += 1
+                else:
+                    broken = True
+                    break 
+            if broken:
+                break    
+        
+        broken = False
+        for x in range(img.shape[1]):
+            for p in range(check_points):
+                y = (p+1) * check_y
+                if img[x,y][0] == 0 and img[x,y][1] == 0:
+                    offset_y[p] += 1   
+                else:
+                    broken = True
+                    break  
+            if broken:
+                break  
+
+        broken = False
+        for y in range(img.shape[0]):
+            y2 = img.shape[0] - (y+1) 
+            for p in range(check_points):
+                x = ((p+1) * check_x)
+                if img[x,y2][0] == 0 and img[x,y2][1] == 0:
+                    offset_x[3 - p] += 1
+                else:
+                    broken = True
+                    break 
+            if broken:
+                break    
+        
+        broken = False
+        for x in range(img.shape[1]):
+            x2 = img.shape[1] - (x +1)
+            for p in range(check_points):
+                y = ((p+1) * check_y)
+                if img[x2,y][0] == 0 and img[x2,y][1] == 0:
+                    offset_y[3 - p] += 1   
+                else:
+                    broken = True
+                    break  
+            if broken:
+                break          
+                         
+        return [offset_x, offset_y]
+
 
     def convertImageToStandardisedBinary(self, img):
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img_gray = cv2.bitwise_not(img_gray) #invert image
         rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
         img_gray = cv2.dilate(img_gray, rect_kernel, iterations = 1)
-        thresh = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+        thresh = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,55,4)
+        white_count = cv2.countNonZero(thresh)
+        black_count = (img.shape[0] * img.shape[1]) - white_count
+        if white_count > black_count:
+            thresh = cv2.bitwise_not(thresh)
         return thresh
         
 
     def getLineLength(self, line):
-        l = line[0]
+        l = line
         x = l[2] - l[0]
         y = l[3] - l[1]
         return math.sqrt(math.pow(x,2) * math.pow(y, 2))    
